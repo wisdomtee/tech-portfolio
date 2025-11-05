fb98a08bc9d69632c4fb0ad0158eeee6da22618a
unknown <wisdomtoyin@gmail.com>
2025-11-05 15:08:28 +0000

    Downgrade Tailwind to v3 for Vercel compatibility

--- tech-portfolio/tech-portfolio/tailwind.config.js
+++ tech-portfolio/tech-portfolio/tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"  // ← Include all JS/JSX files
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
     extend: {},
