c5c2c5302dba9c88534bb4d707cd0f86589776e5
unknown <wisdomtoyin@gmail.com>
2025-11-06 16:37:02 +0000

    Final Netlify build fix

--- tech-portfolio/vite.config.js
+++ tech-portfolio/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ important for Netlify or subfolder builds
+  root: '.', // use current directory
   build: {
     outDir: 'dist',
   },
